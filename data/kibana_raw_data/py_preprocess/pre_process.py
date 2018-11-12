4e7df57c2714f341fc3e2f3d555adaa11cb9a7a0
Sriram Selvam <sriram.selvam@ipinfusion.com>
2018-11-12 17:21:44 -0500

    Sort by timestamp

--- data/kibana_raw_data/py_preprocess/pre_process.py
+++ data/kibana_raw_data/py_preprocess/pre_process.py
@@ -1,12 +1,17 @@
 import json
 import datetime
+import functools as ft
 from pprint import pprint
 
 # Global definitions
 start_msg = "PXEKERNEL/PXEWAIT => PXEKERNEL/PXEWAKEUP"
 end_msg   = "NORMALv2/TBSETUP => NORMALv2/ISUP" 
 
-def get_timestamp_in_datetime (msg):
+def get_timestamp_in_datetime (tstamp_str):
+  date_time_obj = datetime.datetime.strptime (tstamp_str, '%b %d %Y %H:%M:%S')
+  return date_time_obj 
+
+def get_timestamp_in_string (msg):
   words = msg.split()
   date_str = words[0] + " " + words[1] + " 2018 " + words[2]
   date_time_obj = datetime.datetime.strptime (date_str, '%b %d %Y %H:%M:%S')
@@ -16,11 +21,32 @@ def get_timestamp_in_datetime (msg):
 def extract_timestamp_from_msg (hits):
   length = len(hits)
   for i in range (0, length):
-    hits[i]["_source"]["timestamp"] = get_timestamp_in_datetime (hits[i]["_source"]["message"])
-  
+    hits[i]["_source"]["timestamp"] = get_timestamp_in_string (hits[i]["_source"]["message"])
+
+def log_compare (a, b):
+  #Extract the timestamp values to datetime format from both
+  ta = get_timestamp_in_datetime (a["_source"]["timestamp"])
+  tb = get_timestamp_in_datetime (b["_source"]["timestamp"])
+  if (ta < tb):
+    #print ("Found a right aligned one")
+    return -1
+  elif (ta > tb):
+    #print ("Found a wrong aligned one")
+    return 1
+  else:
+    return 0
+
+def sort_log_data_by_time (hits):
+  sorted_hits = sorted (hits, key=ft.cmp_to_key (log_compare))
+  return sorted_hits
+
+def dump_all_messages (hits):
+  length = len(hits)
+  for i in range (0, length):
+    print (hits[i]["_source"]["message"])
+    
 def split_by_start_to_end (hits, start_msg, end_msg):
   length = len(hits)
-  print (length)
 
   #Initialize the variables needed for loop
   new_data = []
@@ -40,6 +66,7 @@ def split_by_start_to_end (hits, start_msg, end_msg):
       start = False
 
       if (log_lines_in_file != 0):
+        new_data.append(hits[i])
         #Write all the data here
         filename = "test" + str(filename_counter) + ".json"
         with open(filename, 'w') as tmp_file:
@@ -65,6 +92,12 @@ def begin_preprocess (data):
   # Since timestamp isn't present in all json objects extract from the message
   extract_timestamp_from_msg (data["hits"]["hits"])
   
+  # Sort all the logs by timestamp
+  data["hits"]["hits"] = sort_log_data_by_time (data["hits"]["hits"])
+
+  #Debug dump
+  #dump_all_messages (data["hits"]["hits"])
+
   # Call file splitter function
   split_by_start_to_end (data["hits"]["hits"], start_msg, end_msg)
 
