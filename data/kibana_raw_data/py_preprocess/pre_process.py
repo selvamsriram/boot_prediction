9da9775133d4db6c786ed894a04643ffbc7d73b8
Sriram Selvam <sriram.selvam@ipinfusion.com>
2018-11-12 16:55:12 -0500

    Preprocessing creates timestamp for all messages

--- data/kibana_raw_data/py_preprocess/pre_process.py
+++ data/kibana_raw_data/py_preprocess/pre_process.py
@@ -1,12 +1,23 @@
 import json
+import datetime
 from pprint import pprint
 
 # Global definitions
 start_msg = "PXEKERNEL/PXEWAIT => PXEKERNEL/PXEWAKEUP"
 end_msg   = "NORMALv2/TBSETUP => NORMALv2/ISUP" 
 
-#def convert_timestamp_to_date (tstamp):
+def get_timestamp_in_datetime (msg):
+  words = msg.split()
+  date_str = words[0] + " " + words[1] + " 2018 " + words[2]
+  date_time_obj = datetime.datetime.strptime (date_str, '%b %d %Y %H:%M:%S')
+  dstr = date_time_obj.strftime ('%b %d %Y %H:%M:%S')
+  return dstr 
 
+def extract_timestamp_from_msg (hits):
+  length = len(hits)
+  for i in range (0, length):
+    hits[i]["_source"]["timestamp"] = get_timestamp_in_datetime (hits[i]["_source"]["message"])
+  
 def split_by_start_to_end (hits, start_msg, end_msg):
   length = len(hits)
   print (length)
@@ -51,6 +62,9 @@ def begin_preprocess (data):
   if data["hits"]["hits"] == None:
     return
 
+  # Since timestamp isn't present in all json objects extract from the message
+  extract_timestamp_from_msg (data["hits"]["hits"])
+  
   # Call file splitter function
   split_by_start_to_end (data["hits"]["hits"], start_msg, end_msg)
 
---
+++ data/kibana_raw_data/py_preprocess/start_preprocess.py
@@ -0,0 +1,1 @@
+python3.6 ./pre_process.py
